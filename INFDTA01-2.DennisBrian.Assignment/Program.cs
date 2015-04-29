acc621a44725120db535f4244c2e131f936bfe08
Denn93 <dennis.wielen@gmail.com>
2015-04-29 11:09:53 +0200

    Get path to Xlsx. Included dataset

--- INFDTA01-2.DennisBrian.Assignment/Program.cs
+++ INFDTA01-2.DennisBrian.Assignment/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace INFDTA01_2.DennisBrian.Assignment
+﻿using System;
+using System.IO;
+
+namespace INFDTA01_2.DennisBrian.Assignment
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("INFDTA Assignments");
+
+            var path = Directory.GetFiles(Directory.GetCurrentDirectory());
+
+            foreach (var s in path)
+                Console.WriteLine(s);
+
+            Console.ReadLine();
         }
     }
 }
